c33ec0479ac7f37cfe24293a8552a7effff12f11
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 16:51:52 -0600

    added 20 working days per month

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -7,29 +7,39 @@ namespace Employee_Wage
         static void Main(string[] args)
         {
             int Wage_Per_Hr = 20;
-            int Employee_Hrs = 0;
+            int monthlyWage = 0;
+            int EmpWage;
             Random rand = new Random();
-            int num = rand.Next(1, 4);
-            Console.WriteLine("Random number is" + num);
-            switch(num)
+            for (int i = 0; i < 20; i++)
             {
-                case 1:
-                    Console.WriteLine("Employee is full time present");
-                    Employee_Hrs = 8;
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is part time present");
-                    Employee_Hrs = 4;
-                    break;
-                case 3:
-                    Console.WriteLine("Employee is absent");
-                    break;
-                default:
-                    Console.WriteLine("correct the program");
-                    break;
+                int Employee_Hrs = 0;
+                int num = rand.Next(1, 4);
+                Console.WriteLine("Random number is :" + num);
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is full time present");
+                        Employee_Hrs = 8;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is part time present");
+                        Employee_Hrs = 4;
+                        break;
+                    case 3:
+                        Console.WriteLine("Employee is absent");
+                        Employee_Hrs = 0;
+                        break;
+                    default:
+                        Console.WriteLine("correct the program");
+                        break;
+                }
+
+                EmpWage = Employee_Hrs * Wage_Per_Hr;
+                monthlyWage += EmpWage;
             }
-            int EmpWage = Employee_Hrs * Wage_Per_Hr;
-            Console.WriteLine("The employee wage is " + EmpWage);
+                Console.WriteLine("The employee wage is " + monthlyWage);
+            
         }
+
     }
 }
