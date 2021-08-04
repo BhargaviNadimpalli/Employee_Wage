a5fb6c143dae4f1337d4621e5c95c1df30f2befb
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 15:44:08 -0600

    feat:added Employee wage

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -6,17 +6,23 @@ namespace Employee_Wage
     {
         static void Main(string[] args)
         {
+            int Wage_Per_Hr = 20;
+            int Employee_Hrs = 0;
             Random rand = new Random();
             int num = rand.Next(0, 2);
             Console.WriteLine("Random number is" + num);
             if (num == 1)
             {
                 Console.WriteLine("Employee is present");
+                Employee_Hrs = 8;
+            
             }
             else
             {
                 Console.WriteLine("Employee is absent");
             }
+            int EmpWage = Employee_Hrs * Wage_Per_Hr;
+            Console.WriteLine("The employee wage is " + EmpWage);
         }
     }
 }
