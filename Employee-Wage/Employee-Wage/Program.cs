c61b9142413bbbf33702879c53437a880386351f
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 16:06:52 -0600

    feat:added part time employee

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -9,16 +9,20 @@ namespace Employee_Wage
             int Wage_Per_Hr = 20;
             int Employee_Hrs = 0;
             Random rand = new Random();
-            int num = rand.Next(0, 2);
+            int num = rand.Next(1, 4);
             Console.WriteLine("Random number is" + num);
             if (num == 1)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is full time present");
                 Employee_Hrs = 8;
-
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("Employee is part time present");
+                Employee_Hrs = 4;
             }
             else
-            {
+            { 
                 Console.WriteLine("Employee is absent");
             }
             int EmpWage = Employee_Hrs * Wage_Per_Hr;
