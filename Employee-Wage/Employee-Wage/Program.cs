85b2a3c62d5306fe40f4b380ef39df52dd20d1e7
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 17:37:51 -0600

    calculated for 100hrs and 20days

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -9,9 +9,13 @@ namespace Employee_Wage
             int Wage_Per_Hr = 20;
             int monthlyWage = 0;
             int EmpWage;
+            int count = 0;
+            int numOfHrsWorked = 0;
+            int daysWorked = 0;
             Random rand = new Random();
-            for (int i = 0; i < 20; i++)
+            while (numOfHrsWorked <= 100 && daysWorked <= 20)
             {
+                EmpWage = 0;
                 int Employee_Hrs = 0;
                 int num = rand.Next(1, 4);
                 Console.WriteLine("Random number is :" + num);
@@ -20,10 +24,14 @@ namespace Employee_Wage
                     case 1:
                         Console.WriteLine("Employee is full time present");
                         Employee_Hrs = 8;
+                        numOfHrsWorked += Employee_Hrs;
+                        daysWorked++;
                         break;
                     case 2:
                         Console.WriteLine("Employee is part time present");
                         Employee_Hrs = 4;
+                        numOfHrsWorked += Employee_Hrs;
+                        daysWorked++;
                         break;
                     case 3:
                         Console.WriteLine("Employee is absent");
@@ -33,10 +41,14 @@ namespace Employee_Wage
                         Console.WriteLine("correct the program");
                         break;
                 }
-
+                Console.WriteLine("Number of hours worked" + numOfHrsWorked);
                 EmpWage = Employee_Hrs * Wage_Per_Hr;
                 monthlyWage += EmpWage;
+                count++;
             }
+
+                Console.WriteLine("Number of times counter ran" + count);
+                Console.WriteLine("Days worked " + daysWorked);
                 Console.WriteLine("The employee wage is " + monthlyWage);
             
         }
