462587a130a6560b161e5d9ea649e4aacf68bcf9
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-04 15:32:13 -0600

    Random check

--- Employee-Wage/Employee-Wage/Program.cs
+++ Employee-Wage/Employee-Wage/Program.cs
@@ -6,7 +6,17 @@ namespace Employee_Wage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee wage folder");
+            Random rand = new Random();
+            int num = rand.Next(0, 2);
+            Console.WriteLine("Random number is" + num);
+            if (num == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
